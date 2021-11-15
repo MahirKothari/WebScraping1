a8f4c01c68041d9f21772d72abde0c51d061baee
MahirKothari <64363014+MahirKothari@users.noreply.github.com>
2021-11-15 17:52:59 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,15 @@
+     for index, li_tag in enumerate(li_tags):
+                if index == 0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            planet_data.append(temp_list)
+        browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+    with open("scrapper_2.csv", "w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(planet_data)
+scrape()
